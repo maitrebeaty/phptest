23a4713a73d8f4c0923c1ccfd37c4bdb6b1718ec
maitrebeaty <39965818+maitrebeaty@users.noreply.github.com>
2022-10-14 14:27:11 -0500

    Create test3.php

---
+++ test3.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    ping 10.1.15.19
+?>
+</pre>
+</body>
+</html>
